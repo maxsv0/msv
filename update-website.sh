ca118341e43e6f05b03b186d78e3495b661e1a68
Max Svistunov <max.svistunov@gmail.com>
2016-12-26 11:50:38 +0200

    add path to argument

--- update-website.sh
+++ update-website.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-echo "UPDATE WEBSITE"
+if [ -z "$1" ]
+  then
+    echo "Please specify installation path"
+fi
+
+echo "Updating MSV Website at $1"
 
 git pull
 
 echo "Copy files"
 
-cp -R src/include/* /var/www/html/include/
+cp -R src/include/* $1/include/
 
-cp -R src/content/* /var/www/html/content/
+cp -R src/content/* $1/content/
 
-cp -R src/templates/* /var/www/html/templates/
+cp -R src/templates/* $1/templates/
 
-cp -R src/index.php /var/www/html/index.php
+cp -R src/index.php $1/index.php
 
-cp -R src/load.php /var/www/html/load.php
+cp -R src/load.php $1/load.php
+
+rm -R $1/include/module/install/
