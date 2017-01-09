e8bdfa4d884a5bee4f38d50fcfa808184e54af77
Max Svistunov <max.svistunov@gmail.com>
2017-01-09 16:35:03 +0200

    +design management +design-default

--- src/include/module/msv-core/msv-core.php
+++ src/include/module/msv-core/msv-core.php
@@ -1527,24 +1527,23 @@ function MSV_PasswordGenerate($length = 12) {
 
 
 function CoreInstall($module) {
-	
-	MSV_Structure_add("all", "/", _t("structure.homepage"), "custom", "index.tpl", 1, "top", 1, "everyone");
-	MSV_Structure_add("all", "/the-first-page/", _t("structure.first_page"), "custom", "main.tpl", 1, "top", 2, "everyone");
-	MSV_Structure_add("all", "/404/", _t("structure.404"), "default", "404.tpl", 0, "", 0, "everyone");
-	MSV_Structure_add("*", "/admin/", _t("structure.admin_homepage"), "default", "admin.tpl", 0, "", 0, "admin");
-	MSV_Structure_add("*", "/admin/login/", _t("structure.admin_login"), "default", "admin-login.tpl", 0, "", 0, "everyone");
-	
+
 	// add site settings
 	MSV_setConfig("include_html_head", "", true, "*");
-	MSV_setConfig("theme_css_path", "/content/css/default.css", true, "*");
-	MSV_setConfig("theme_js_path", "/content/js/default.js", true, "*");
-	MSV_setConfig("theme_include_font", "<link href='https://fonts.googleapis.com/css?family=Roboto:300,500&subset=latin,cyrillic' rel='stylesheet' type='text/css'>", true, "*");
-	MSV_setConfig("theme_use_bootstrap", 1, true, "*");
-	MSV_setConfig("theme_use_jquery", 1, true, "*");
+	MSV_setConfig("theme_css_path", "", true, "*");
+	MSV_setConfig("theme_js_path", "", true, "*");
+	MSV_setConfig("theme_include_font", "", true, "*");
+	MSV_setConfig("theme_use_bootstrap", 0, true, "*");
+	MSV_setConfig("theme_use_jquery", 0, true, "*");
+	
+	// trigger email sending on user registration
 	MSV_setConfig("email_registration", 0, true, "*");
+	
+	// add messages to default output
 	MSV_setConfig("message_ok", "", true, "*");
-	MSV_setConfig("message_error", _t("msg.development_version"), true, "*");
-	//sss
+	MSV_setConfig("message_error", "", true, "*");
+	
+	// mailing options
 	MSV_setConfig("email_from", "", true, "*");
 	MSV_setConfig("email_fromname", "", true, "*");
 	
