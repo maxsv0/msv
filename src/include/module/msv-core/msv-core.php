e38b52d1c1e387a5cb2385aa9d2721513bb1675f
Max Svistunov <max.svistunov@gmail.com>
2017-01-21 20:39:35 +0200

    + MSV_getIP()

--- src/include/module/msv-core/msv-core.php
+++ src/include/module/msv-core/msv-core.php
@@ -1534,7 +1534,20 @@ function MSV_PasswordGenerate($length = 12) {
     return $result;
 }
 
-		
+
+function MSV_GetIP() {
+	
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])){
+		$ip  =$_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+	
+	return $ip;
+}
+
 
 
 
